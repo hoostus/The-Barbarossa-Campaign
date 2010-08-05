58a219aa86508df216b989e69ff1e2a6f84ce4b0
Justus Pendleton <justus@ryoohki.net>
2010-08-05 21:42:38 +1000

    add pnotify and hijack the browser alert

--- js/ui-setup.js
+++ js/ui-setup.js
@@ -1,4 +1,14 @@
 jQuery(document).ready(function(){
 	jQuery('input:submit').button()
 	jQuery('input:checkbox').button()
+
+	// hijack the browser alert with pnotify
+	if (_alert) return;
+	var _alert = window.alert;
+	window.alert = function(message) {
+		jQuery.pnotify({
+			pnotify_title: 'Alert',
+			pnotify_text: message
+		});
+	}
 });
